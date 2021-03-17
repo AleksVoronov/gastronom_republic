28c138e14bdc56833007ba45e8d97bc41f1245f2
AleksVoronov <av591955@gmail.com>
2021-03-17 22:39:08 +0200

    first commit

--- gastronomrepublic/settings.py
+++ gastronomrepublic/settings.py
@@ -13,6 +13,7 @@ DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 import os
 import posixpath
 import dj_database_url
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
--- gastronomrepublic/wsgi.py
+++ gastronomrepublic/wsgi.py
@@ -27,6 +27,8 @@ os.environ.setdefault(
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 application = get_wsgi_application()
+from whitenoise.django import DjangoWhiteNoise
+application = DjangoWhiteNoise(application)
 
 #application = DjangoWhiteNoise(application)
 
